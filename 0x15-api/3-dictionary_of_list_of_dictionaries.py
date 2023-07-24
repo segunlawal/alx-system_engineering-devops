025ec6fff58d332cd407ea7b8f997053ca5a0dac
Segun <lawalsegun72@gmail.com>
2023-07-24 16:10:43 -0700

    export in JSON fomat

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Python script gathers data from an API"""
+
+import json
+import requests
+
+
+def get_list():
+    """Get and display info about todo list progress"""
+    url = "https://jsonplaceholder.typicode.com/users"
+
+    response = requests.get(url)
+    users = response.json()
+
+    dictionary = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        url = url + '/todos/'
+        response = requests.get(url)
+        tasks = response.json()
+        dictionary[user_id] = []
+        for task in tasks:
+            dictionary[user_id].append({
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": username
+            })
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(dictionary, file)
+
+
+if __name__ == "__main__":
+    get_list()
